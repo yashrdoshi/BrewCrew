19b346e1558badd014be92e1c33aae9771ce04af
yashrdoshi <yashdoshi99.04@gmail.com>
2021-01-01 21:46:22 +0530

    brew_crew

---
+++ lib/screens/authenticate/register.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:brew_crew/services/auth.dart';
+import 'package:brew_crew/shared/constants.dart';
+import 'package:brew_crew/shared/loading.dart';
+
+class Register extends StatefulWidget {
+
+  final Function toggleView;
+  Register({this.toggleView});
+
+
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+
+  final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
+  bool loading = false;
+
+
+   //text field State
+  String email = "";
+  String password = "";
+  String error = "";
+
+
+  @override
+  Widget build(BuildContext context) {
+    return loading? Loading():Scaffold(
+      backgroundColor: Colors.brown[100],
+      appBar: AppBar(
+        backgroundColor: Colors.brown[400],
+        elevation: 0.0,
+        title: Text("Sign up to Brew Crew"),
+         actions: <Widget>[
+          FlatButton.icon(
+            onPressed: (){
+              widget.toggleView();
+            }, 
+            icon: Icon(Icons.person), 
+            label: Text("Sign In"),
+            )
+        ],
+      ),
+      body: SingleChildScrollView(
+              child: Container(
+          padding: EdgeInsets.symmetric(vertical: 20.0,horizontal: 50.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: 20.0,),
+                TextFormField(
+                  decoration: textInputDecoration.copyWith(hintText: "Email"),
+                  validator: (val) => val.isEmpty? "Enter an E-mail":null,
+                  onChanged: (val){
+                      setState(() {
+                        email = val;
+                      });
+                  },
+                ),
+                SizedBox(height: 20.0,),
+                TextFormField(
+                  decoration: textInputDecoration.copyWith(hintText: "Password"),
+                  obscureText: true,
+                  validator: (val) => val.length < 6? "Enter a password 6+ chracters long":null,
+                  onChanged: (val){
+                    setState(() {
+                      password = val;
+                    });
+                  },
+                ),
+                SizedBox(height: 20.0,),
+                RaisedButton(
+                  color: Colors.pink[400],
+                  child: Text(
+                    "Register",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: ()async{
+                    if(_formKey.currentState.validate()){
+                      setState(() {
+                        loading = true;
+                      });
+                      dynamic result = await _auth.registerWithEmailAndPassword(email,password);
+                      if(result == null)
+                        setState(() {
+                          loading = false;
+                          error = "Please supply a valid email";
+                        });
+                    }
+                  },
+                ),
+                SizedBox(height: 20.0,),
+                Text(
+                  error,
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 14.0,
+                     ),
+               )              
+              ],
+              ),
+            )
+        ),
+      ),
+    );
+  }
+}
