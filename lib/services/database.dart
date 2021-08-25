04107138b760cf4bb34670fff0d018fefb470592
yashrdoshi <yashdoshi99.04@gmail.com>
2021-08-25 19:12:47 +0530

    logo

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -2,55 +2,50 @@ import 'package:brew_crew/models/brew.dart';
 import 'package:brew_crew/models/user.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class DatabaseService{
-
+class DatabaseService {
   final String uid;
 
   DatabaseService({this.uid});
 
   //collection reference
 
-  final CollectionReference brewCollection = Firestore.instance.collection("brew");
+  final CollectionReference brewCollection =
+      Firestore.instance.collection("brew");
 
-  Future updateUserData(String sugars,String name,int strength)async{
-    return await brewCollection.document(uid).setData({
-      'sugars' : sugars,
-      'name': name,
-      'strength': strength
-    });
+  Future updateUserData(String sugars, String name, int strength) async {
+    return await brewCollection
+        .document(uid)
+        .setData({'sugars': sugars, 'name': name, 'strength': strength});
   }
 
   //brew list from Snapshots
-  List<Brew> _brewListFromSanpshot(QuerySnapshot snapshot){
-    return snapshot.documents.map((doc){
+  List<Brew> _brewListFromSanpshot(QuerySnapshot snapshot) {
+    return snapshot.documents.map((doc) {
       return Brew(
-        name:doc.data['name']??"",
+        name: doc.data['name'] ?? "",
         sugars: doc.data['sugars'] ?? '0',
-        strength: doc.data['strength']?? 0,
+        strength: doc.data['strength'] ?? 0,
       );
     }).toList();
   }
   //user data from snapshots
 
-  UserData _userDataFromSnapshot(DocumentSnapshot snapshot){
+  UserData _userDataFromSnapshot(DocumentSnapshot snapshot) {
     return UserData(
       uid: uid,
       name: snapshot.data['name'],
       sugars: snapshot.data['sugars'],
       strength: snapshot.data['strength'],
-      );
-  }
-  //get brews Stream
-  Stream<List<Brew>> get brews{
-    return brewCollection.snapshots()
-          .map(_brewListFromSanpshot);
+    );
   }
 
+  //get brews Stream
+  Stream<List<Brew>> get brews {
+    return brewCollection.snapshots().map(_brewListFromSanpshot);
+  }
 
   //get user doc stream
-  Stream<UserData> get userData{
-    return brewCollection.document(uid).snapshots()
-          .map(_userDataFromSnapshot);
+  Stream<UserData> get userData {
+    return brewCollection.document(uid).snapshots().map(_userDataFromSnapshot);
   }
-
 }
